d8ac33accf589e69b4b1d614d5e2d0fce7607746
Geon Yoo <46584775+jasongeon123@users.noreply.github.com>
2021-11-10 15:39:33 -0800

    11/10/2021

--- script.js
+++ script.js
@@ -7,20 +7,6 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
-        
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
-
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
     });
 
     $('.navbar .menu li a').click(function(){
@@ -28,11 +14,6 @@ $(document).ready(function(){
         $('html').css("scrollBehavior", "smooth");
     });
 
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
     var typed = new Typed(".typing", {
         strings: ["Gamer", "Developer", "Student", "Designer", "Wonderer"],
         typeSpeed: 100,
