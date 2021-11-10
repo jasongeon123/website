f0034c9e262c7119e8fce2489c1ab2143c2bd84a
Geon Yoo <46584775+jasongeon123@users.noreply.github.com>
2021-11-10 15:02:27 -0800

    it

--- Root/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["Gamer", "Developer", "Student", "Designer", "Wonderer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-});
--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<?php include_once("./Root/index.html"); ?>
+<?php include_once("index.html"); ?>
