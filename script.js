a9b5481187690bf31f16a6e5b6a867ece29db731
Geon Yoo <46584775+jasongeon123@users.noreply.github.com>
2021-11-15 01:00:44 -0800

    11/15/2021

--- script.js
+++ script.js
@@ -7,6 +7,28 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
     });
 
     $('.navbar .menu li a').click(function(){
@@ -20,6 +42,7 @@ $(document).ready(function(){
         backSpeed: 75,
         loop: true
     });
+
 
     
 });
