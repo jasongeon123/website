8313e1e1b631fc017ce056e6d6acc4c9b4d2ce99
Geon Yoo <46584775+jasongeon123@users.noreply.github.com>
2022-05-05 12:41:11 -0700

    kloak case

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
     // typing text animation script
     $(window).scroll(function(){
         // sticky navbar on scroll script
-        if(this.scrollY > 0){
+        if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
         }
-        if(this.scrollY > 0){
+        if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
         }else{
             $('.scroll-up-btn').removeClass("show");
@@ -20,6 +20,10 @@ $(document).ready(function(){
         $('html').css("scrollBehavior", "auto");
     });
 
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
 
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
@@ -27,7 +31,10 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass("active");
     });
 
-
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
 
     var typed = new Typed(".typing", {
         strings: ["Gamer", "Developer", "Student", "Designer", "Golfer"],
